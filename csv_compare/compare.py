4cd2b1d4bf61532ba0dabfb252b980f55bde9f61
Admin <admin@Admins-MBP.lan>
2024-04-06 01:04:11 +0200

    initial commit

---
+++ csv_compare/compare.py
@@ -0,0 +1,8 @@
+from csv_compare.csvfile import CSVFile
+
+
+def compare(filename1: str, filename2: str) -> bool:
+    file1 = CSVFile(filename1)
+    file2 = CSVFile(filename2)
+
+    return file1.read().equals(file2.read())
